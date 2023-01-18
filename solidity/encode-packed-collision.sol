ea44ecdda580cf804beabd31138af8e646da865b
raz0r <me@raz0r.name>
2023-01-18 12:02:44 +0300

    add one more test case, allow a gap between args

--- solidity/encode-packed-collision.sol
+++ solidity/encode-packed-collision.sol
@@ -1,8 +1,14 @@
 contract Test {
   
-  function f(string memory a, bytes b) public pure returns (bytes memory) {
+  function f(string memory a, bytes memory b) public pure returns (bytes32) {
     // ruleid: encode-packed-collision
     return keccak256(abi.encodePacked(a, b));
+  }
+
+  function f2(bytes memory a, bytes memory b) public pure returns (bytes32) {
+    // ruleid: encode-packed-collision
+    bytes memory x = abi.encodePacked(a, b);
+    return keccak256(x);
   }
 
   function query(
