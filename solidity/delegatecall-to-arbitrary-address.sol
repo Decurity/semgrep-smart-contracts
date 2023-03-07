d98aec52e24577008ccaacb06f42c07e4f76bef2
frodan <daniil.fronc@mail.ru>
2023-03-07 20:32:33 +0530

    Update delegatecall-to-arbitrary-address.sol

--- solidity/delegatecall-to-arbitrary-address.sol
+++ solidity/delegatecall-to-arbitrary-address.sol
@@ -3,21 +3,21 @@ pragma solidity 0.8.0;
 
 contract Test{
     function func1(address _contract, uint256 _num) external{
-        //ruleid:delegatecall-controlled-by-user
+        //ruleid: delegatecall-to-arbitrary-address
         (bool success, bytes memory data) = _contract.delegatecall(
             abi.encodeWithSignature("setVars(uint256)", _num)
         );
     }
 
     function func2(address payable _contract, uint256 _num) public{
-        //ruleid:delegatecall-controlled-by-user
+        //ruleid: delegatecall-to-arbitrary-address
         (bool success, bytes memory data) = _contract.delegatecall(
             abi.encodeWithSignature("setVars(uint256)", _num)
         );
     }
 
     function func3(uint256 useless, address  _contract, uint256 _num) external{
-        //ruleid:delegatecall-controlled-by-user
+        //ruleid: delegatecall-to-arbitrary-address
         (bool success, bytes memory data) = _contract.delegatecall(
             abi.encodeWithSignature("setVars(uint256)", _num)
         );
