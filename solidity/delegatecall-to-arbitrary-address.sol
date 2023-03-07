4a0e47490067bdfb4a5a33c766303c37d9bbfe07
frodan <daniil.fronc@mail.ru>
2023-03-07 20:07:36 +0530

    + rule

---
+++ solidity/delegatecall-to-arbitrary-address.sol
@@ -0,0 +1,25 @@
+pragma solidity 0.8.0;
+
+
+contract Test{
+    function func1(address _contract, uint256 _num) external{
+        //ruleid:delegatecall-controlled-by-user
+        (bool success, bytes memory data) = _contract.delegatecall(
+            abi.encodeWithSignature("setVars(uint256)", _num)
+        );
+    }
+
+    function func2(address payable _contract, uint256 _num) public{
+        //ruleid:delegatecall-controlled-by-user
+        (bool success, bytes memory data) = _contract.delegatecall(
+            abi.encodeWithSignature("setVars(uint256)", _num)
+        );
+    }
+
+    function func3(uint256 useless, address  _contract, uint256 _num) external{
+        //ruleid:delegatecall-controlled-by-user
+        (bool success, bytes memory data) = _contract.delegatecall(
+            abi.encodeWithSignature("setVars(uint256)", _num)
+        );
+    }
+}
