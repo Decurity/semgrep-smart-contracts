5ea7ff413d5060db5f89bceb806cd038c3349cb4
h1kk4 <ipodmacair@gmail.com>
2023-03-20 13:00:43 +0400

    added new gaz rules

---
+++ solidity/use-short-revert-string.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.2;
+
+contract TestRequiere {
+    function test(uint256 a) public {
+        // ruleid: use-short-revert-string
+        require(a>10, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"); // "a"*33
+    }
+
+    function test2(uint256 a) public {
+        // ok: use-short-revert-string
+        require(a>10, "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"); // "a"*32
+    }
+}
