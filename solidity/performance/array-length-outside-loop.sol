8017e49e68dd9501991f7852fffbc6fba888e069
h1kk4 <ipodmacair@gmail.com>
2023-05-30 18:48:43 +0600

    storage variable length should be cached too (#34)
    Co-authored-by: Raz0r <me@raz0r.name>

--- solidity/performance/array-length-outside-loop.sol
+++ solidity/performance/array-length-outside-loop.sol
@@ -12,7 +12,8 @@ contract Test {
 }
 
 contract Test2 {
-    address[] all_data;
+    address[2] all_data = [address(0), address(0)];
+    address[] all_data_2;
     function doSmthng() public returns(uint256) {
         uint256 len = array.length;
         // ok: array-length-outside-loop
@@ -29,12 +30,21 @@ contract Test2 {
     }
 
     function doSmthng3() public returns(uint256) {
+        address[2] storage arr = all_data;
         // ok: array-length-outside-loop
-        address[] storage arr = all_data;
         for (uint256 i = 0; i < arr.length;) {
             // invariant: array's length is not changed
         }
     }
+
+    function doSmthng4() public returns(uint256) {
+        address[] storage arr = all_data_2;
+        // ruleid: array-length-outside-loop
+        for (uint256 i = 0; i < arr.length;) {
+            // invariant: array's length is not changed
+        }
+    }
+
 }
 
 contract Test3 {
